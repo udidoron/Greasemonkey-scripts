4c94ab40d5967864caf2e0e56f43bf3fa744b649
udidoron <ehud.doron@mail.huji.ac.il>
2015-02-14 01:11:46 +0200

    XKCD What-If script: added keyboard navigation, and organized the code into functions.

--- XKCD_What-If_Display_picture_alt-texts_on_screen.user.js
+++ XKCD_What-If_Display_picture_alt-texts_on_screen.user.js
@@ -8,13 +8,44 @@
 // @grant       none
 // ==/UserScript==
 
-var pics = document.getElementsByClassName("illustration");
-for (var i=0; i<pics.length; i++) { 
-     var text = document.createElement("div");
-     text.style.backgroundColor = "lightblue";
-     text.style.color="black";
-     text.style.textAlign="center";
-     text.innerHTML = pics[i].title;
-     pics[i].parentNode.insertBefore(text, pics[i].nextElementSibling);
+function addAltTexts() {
+     var pics = document.getElementsByClassName("illustration");
+     for (var i=0; i<pics.length; i++) { 
+          var text = document.createElement("div");
+          text.style.backgroundColor = "lightblue";
+          text.style.color="black";
+          text.style.textAlign="center";
+          text.innerHTML = pics[i].title;
+          pics[i].parentNode.insertBefore(text, pics[i].nextElementSibling);
+     }
+
 }
+
+function addKeyboardNavigation() {
+     var currentPage = document.location.pathname.replace("/", "").replace("/", ""); //a bit ugly, but works
+     var lastPage = (document.getElementsByClassName("nav-next").length === 0);
+     var firstPage = (currentPage == "1");
+     if (!firstPage && !lastPage) {
+          currentPage = parseInt(currentPage);
+     }
+     
+     document.addEventListener("keypress", function(event) {
+          //'left' key code = 37, 'right' key code = 39
+          if (event.keyCode === 37 && !firstPage) {
+               document.location="http://what-if.xkcd.com/"+String(currentPage-1);
+          }
+          else if (event.keyCode === 39 && !lastPage) {
+               if (currentPage == "1") {
+                   document.location="http://what-if.xkcd.com/2"; //to fix stupid bug at 1:02 AM, which only appears for first page, dunno why  
+               }
+               else {
+                   document.location="http://what-if.xkcd.com/"+String(currentPage+1);    
+               }
+               
+          }
+     });
+}
+
+addAltTexts();
+addKeyboardNavigation();
 
